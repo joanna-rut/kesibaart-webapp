0e574f43dd9347b99ed296cf7644cd75f68aad5b
mohamed tayeb <pipealoha1@gmail.com>
2025-10-29 19:26:49 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -3,12 +3,15 @@
 import type { ReactNode } from 'react';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
+import { PageTransition } from '@/components/layout/PageTransition';
 
 export function ClientLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <PageTransition>
+        <main>{children}</main>
+      </PageTransition>
       <Footer />
     </>
   );
