7dbde84ce76a9b13751247d27e8b8f1ed0911784
mohamed tayeb <pipealoha1@gmail.com>
2025-10-29 17:58:47 +0000

    surprise me

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -7,45 +7,46 @@ const socialLinks = [
     name: "Instagram",
     href: "https://www.instagram.com/kesibaart/",
     icon: Instagram,
-    color: "bg-[#E4405F] hover:bg-[#E4405F]/90",
+    color: "text-[#E4405F] hover:bg-[#E4405F] hover:text-white",
   },
   {
     name: "Facebook",
     href: "https://www.facebook.com/share/15TmLcy6WD/",
     icon: Facebook,
-    color: "bg-[#1877F2] hover:bg-[#1877F2]/90",
+    color: "text-[#1877F2] hover:bg-[#1877F2] hover:text-white",
   },
   {
     name: "Etsy",
     href: "https://www.etsy.com/shop/kesibaart/",
     icon: EtsyIcon,
-    color: "bg-[#F16521] hover:bg-[#F16521]/90",
+    color: "text-[#F16521] hover:bg-[#F16521] hover:text-white",
   },
   {
     name: "eBay",
     href: "https://www.ebay.ca/sch/i.html?item=176714948962&rt=nc&_trksid=p4429486.m3561.l161211&_ssn=kesiba",
     icon: EBayIcon,
-    color: "bg-[#000000] hover:bg-[#000000]/90",
+    color: "text-black dark:text-white hover:bg-black hover:text-white",
   },
 ];
 
 export default function SocialLinks() {
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
       {socialLinks.map((link) => (
         <Button
           key={link.name}
           asChild
-          className={`w-full text-base py-6 rounded-lg shadow-md transition-transform hover:scale-105 ${link.color} text-white`}
+          variant="outline"
+          className={`w-full text-base py-8 rounded-lg shadow-sm transition-colors duration-300 ${link.color} border-primary/20`}
         >
           <a
             href={link.href}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center"
+            className="flex flex-col items-center justify-center gap-2"
           >
-            <link.icon className="mr-3 h-6 w-6" />
-            <span>{link.name}</span>
+            <link.icon className="h-8 w-8" />
+            <span className="font-semibold">{link.name}</span>
           </a>
         </Button>
       ))}
